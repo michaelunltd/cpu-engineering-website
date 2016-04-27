1edf47e5e3f9116af4d8c0155b8666e80a258119
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2016-04-27 17:03:12 +0800

    News crud 80% complete

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,5 @@
 class NewsController < ApplicationController
-
+  before_action :find_news, only: [:show, :edit, :destroy, :update]
   def index
     @news = News.all
   end
@@ -8,23 +8,60 @@ class NewsController < ApplicationController
   end
 
   def new
+    @news = News.new
   end
 
   def create
+    @title = params[:news][:title]
+    @content = params[:news][:content]
+    @author = params[:news][:author]
+    @real_img_path = params[:news][:img_path].path
+
+    @news = News.new(
+      title: @title,
+      content: @content,
+      img_path: @real_img_path,
+      author: @author
+    )
+
+
+    if @news.save
+      redirect_to news_index_path
+    else
+      flash[:notice] = 'News WAS NOT added.'
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @title = params[:news][:title]
+    @content = params[:news][:content]
+    @author = params[:news][:author]
+    @real_img_path = params[:news][:img_path].path
+
+    if @news.update( title: @title, content: @content, img_path: @real_img_path, author: @author)
+      redirect_to news_index_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @news.destroy
+
+    redirect_to news_index_path
   end
 
 private
 
   def news_params
-    params.require(:client).permit(:title, :content, :img_path, :author)
+    params.require(:news).permit(:title, :content, :img_path, :author)
+  end
+
+  def find_news
+    @news = News.find params[:id]
   end
 end
