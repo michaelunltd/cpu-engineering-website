acb9b22fd9ce6454ab51fa0c56d75b19022bcfdb
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2016-05-02 14:22:17 +0800

    Department crud

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428032402) do
+ActiveRecord::Schema.define(version: 20160502052419) do
+
+  create_table "departments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "about"
+    t.text     "message"
+    t.text     "vision"
+    t.text     "mission"
+    t.text     "history"
+    t.string   "logo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "venue"
+    t.string   "image"
+    t.date     "startDate"
+    t.date     "endDate"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "news", force: :cascade do |t|
     t.string   "title"
