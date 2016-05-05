c44f9e05e1a10c5e2c77e648ab2943731e860efb
Patrick Rainier R. Juen <patrickrainier@gmail.com>
2016-05-05 14:31:28 +0800

    finished outreach programs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504022311) do
+ActiveRecord::Schema.define(version: 20160505055938) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -81,6 +81,13 @@ ActiveRecord::Schema.define(version: 20160504022311) do
     t.string   "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "outreach_programs", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
