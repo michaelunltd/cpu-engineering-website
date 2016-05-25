6f161448aca2caddbfd12ba30ebb7ab161ed8fff
Patrick Rainier R. Juen <patrickrainier@gmail.com>
2016-05-25 12:39:24 +0800

    adding seeder for departments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513055525) do
+ActiveRecord::Schema.define(version: 20160519065855) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "actable_id"
@@ -133,6 +133,13 @@ ActiveRecord::Schema.define(version: 20160513055525) do
   end
 
   add_index "researches", ["department_id"], name: "index_researches_on_department_id"
+
+  create_table "static_pages", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",         null: false
