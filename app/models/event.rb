5c37faae323776e734c112bcb7bc7a3d8b19bc00
Patrick Rainier R. Juen <patrickrainier@gmail.com>
2016-06-09 14:10:36 +0800

    added validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ActiveRecord::Base
 	acts_as :article
+
+  validate :start_date_cannot_be_in_the_past
+
+  def start_date_cannot_be_in_the_past
+    if start_date < DateTime.now()
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+
 end
