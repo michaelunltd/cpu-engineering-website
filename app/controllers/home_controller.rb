4b135144e16f116e6a027d9a35661001399ea22c
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2016-06-11 23:00:48 +0800

    Home news added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
     def index
         @articles = Article.where 'featured = ?', true
+        @news = News.order(created_at: :desc).first(3)
     end
 end
